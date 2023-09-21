1af2f44b751d923b28e6195ca974978cabf49401
alvinseyidov <alvinseyidov@gmail.com>
2023-09-21 16:44:32 +0400

    update

--- offer/admin.py
+++ offer/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(OffersPage)
+
 
 class OfferImageInline(admin.TabularInline):
     model = OfferImage
--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(ProductsPage)
+
 class ProductImageInline(admin.TabularInline):
     model = ProductImage
 
