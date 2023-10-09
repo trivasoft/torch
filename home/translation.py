06bc0c49d146e4bc245615be4f55038b2df82e52
alvinseyidov <alvinseyidov@gmail.com>
2023-10-09 22:57:41 +0400

    update

--- home/translation.py
+++ home/translation.py
@@ -6,3 +6,13 @@ class GeneralTranslationOptions(TranslationOptions):
     fields = ('address','sirketimiz','copyright','our_mission','deyerlerimiz','korporativ_medeniyyet','sirketimiz_title_text','our_mission_title_text','deyerlerimiz_title_text','korporativ_medeniyyet_title_text')
 
 translator.register(General, GeneralTranslationOptions)
+
+class TeamCatTranslationOptions(TranslationOptions):
+    fields = ('name',)
+
+translator.register(TeamCategory, TeamCatTranslationOptions)
+
+class TeamTranslationOptions(TranslationOptions):
+    fields = ('position',)
+
+translator.register(Team, TeamTranslationOptions)
