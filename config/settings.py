0794573735da443bc4a024c50471fcdd86b94661
Elvin Seyidov <alvinseyidov@gmail.com>
2024-02-05 18:53:12 +0400

    update

--- config/settings.py
+++ config/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -92,7 +92,7 @@ DATABASES = {
         'PORT': '',
     }
 }
-'''
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
